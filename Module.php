705427d2713511b014c95554999b82818b9f4dfc
nadine-shi <nadine@afterlogic.com>
2019-04-26 14:45:27 +0300

    License and other texts improving

--- Module.php
+++ Module.php
@@ -1,9 +1,21 @@
 <?php
+/**
+ * This code is licensed under AGPLv3 license or Afterlogic Software License
+ * if commercial version of the product was purchased.
+ * For full statements of the licenses see LICENSE-AFTERLOGIC and LICENSE-AGPL3 files.
+ */
 
 namespace Aurora\Modules\Licensing;
 
 include_once __DIR__.'/classes/KI.php';
 
+/**
+ * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
+ * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
+ * @copyright Copyright (c) 2019, Afterlogic Corp.
+ *
+ * @package Modules
+ */
 class Module extends \Aurora\System\Module\AbstractModule
 {
 	protected $key = null;
