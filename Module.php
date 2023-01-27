37135e5806629a0b7d9063e3bc23e6dab2bf6209
Vasil Sokolov <vasil@afterlogic.com>
2023-01-27 12:45:56 +0200

    Added CS Fixer check to pre-commit

--- Module.php
+++ Module.php
@@ -18,282 +18,251 @@ include_once __DIR__.'/classes/KI.php';
  */
 class Module extends \Aurora\System\Module\AbstractModule
 {
-	protected $key = null;
+    protected $key = null;
 
-	protected $keyInfo = false;
+    protected $keyInfo = false;
 
-	protected $bIsPermanent = true;
+    protected $bIsPermanent = true;
 
-	public function init() {}
+    public function init()
+    {
+    }
 
-	public function ResetKey()
-	{
-		$this->key = null;
-		$this->keyInfo = false;
-	}
+    public function ResetKey()
+    {
+        $this->key = null;
+        $this->keyInfo = false;
+    }
 
-	public function GetLicenseKey()
-	{
-		if (!isset($this->key))
-		{
-			$oSettings = \Aurora\System\Api::GetSettings();
-			if ($oSettings)
-			{
-				$this->key = $oSettings->GetValue('LicenseKey', '');
-				\Aurora\System\Api::AddSecret($this->key);
-			}
-		}
-		
-		return $this->key;
-	}
+    public function GetLicenseKey()
+    {
+        if (!isset($this->key)) {
+            $oSettings = \Aurora\System\Api::GetSettings();
+            if ($oSettings) {
+                $this->key = $oSettings->GetValue('LicenseKey', '');
+                \Aurora\System\Api::AddSecret($this->key);
+            }
+        }
 
-	public function GetLicenseKeyPrefix()
-	{
-		$sKey = Module::Decorator()->GetLicenseKey();
-		$aParts = \explode('-', $sKey);
-		return \array_shift($aParts);
-	}
-	
-	protected function getKeyInfo()
-	{
-		if (!$this->keyInfo)
-		{
-			$sKey = Module::Decorator()->GetLicenseKey();
-		
-			if (!empty($sKey))
-			{
-				$oKI = new \KI($this->GetLicenseKeyPrefix());
-				$this->keyInfo = $oKI->GKI($sKey);
-			}
-		}
-		
-		return $this->keyInfo;
-	}
-	
-	protected function GetPartKeyInfo($sPart = 'System')
-	{
-		$mResult = false;
-		$aKeyInfo = $this->getKeyInfo();
-		if (isset($aKeyInfo[$sPart]))
-		{
-			$mResult = $aKeyInfo[$sPart];
-		}		
+        return $this->key;
+    }
 
-		return $mResult;
-	}
-	
-	protected function GetPartKeyData($sPart, $iPosition)
-	{
-		$mResult = false;
-		$aModuleKeyInfo = $this->GetPartKeyInfo($sPart);
-		
-		if ($aModuleKeyInfo)
-		{
-			if (isset($aModuleKeyInfo[$iPosition]) && $aModuleKeyInfo[$iPosition] !== '*')
-			{
-				$mResult = $aModuleKeyInfo[$iPosition];
-			}
-			else
-			{
-				$aModuleKeyInfo = $this->GetPartKeyInfo('System');
-				if ($aModuleKeyInfo && isset($aModuleKeyInfo[$iPosition]))
-				{
-					$mResult = $aModuleKeyInfo[$iPosition];
-				}
-			}
-		}
-		
-		return $mResult;
-	}
+    public function GetLicenseKeyPrefix()
+    {
+        $sKey = Module::Decorator()->GetLicenseKey();
+        $aParts = \explode('-', $sKey);
+        return \array_shift($aParts);
+    }
 
-	public function GetUsersCount($Module)
-	{
-		return $this->GetPartKeyData($Module, 0);
-	}
-	
-	public function Validate($sModule)
-	{
-		return !!$this->GetPartKeyInfo($sModule) ? true : $this->IsTrial() && !!$this->GetPartKeyInfo();
-	}
-	
-	public function ValidateUsersCount($iCount, $sModule = 'System')
-	{
-		$bResult = true;
-		$aInfo = $this->GetPartKeyInfo($sModule);
-		if (is_array($aInfo))
-		{
-			$iType = (int) $aInfo[2];
+    protected function getKeyInfo()
+    {
+        if (!$this->keyInfo) {
+            $sKey = Module::Decorator()->GetLicenseKey();
 
-			if ($iType !== 10 && $iType !== 0)
-			{
-				$bResult = ($iCount < $this->GetUsersCount($sModule));
-			}
-		}
-		else
-		{
-			$bResult = false;
-		}
-		
-		return $bResult;
-	}
-	
-	public function ValidatePeriod($sModule = 'System')
-	{
-		$bResult = false;
-		
-		$sPeriod = $this->GetPartKeyData($sModule, 1);
-		$sType= $this->GetPartKeyData($sModule, 2);
-				
-		if (isset($sType))
-		{
-			$iType = (int) $sType;
-			if (isset($sPeriod) && ($iType === 3 || $iType === 4 || $iType === 10))
-			{
-				$iPeriod = (int) $sPeriod;
-				$bResult = ($iPeriod - time()) > 0;
-			}
-			else
-			{
-				$bResult = true;
-			}
-		}
-		
-		return $bResult;
-	}
+            if (!empty($sKey)) {
+                $oKI = new \KI($this->GetLicenseKeyPrefix());
+                $this->keyInfo = $oKI->GKI($sKey);
+            }
+        }
 
-	public function GetSettings()
-	{
-		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::SuperAdmin);
+        return $this->keyInfo;
+    }
 
-		return array(
-			'LicenseKey' => Module::Decorator()->GetLicenseKey()
-		);
-	}
-	
-	/**
-	 * @return bool
-	 */
-	public function UpdateSettings($LicenseKey)
-	{
-		$bResult = false;
-		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::SuperAdmin);
-		
-		if ($LicenseKey !== null)
-		{
-			\Aurora\System\Api::GetSettings()->LicenseKey = $LicenseKey;
-			$bResult = \Aurora\System\Api::GetSettings()->Save();
-		}
-		
-		return $bResult;
-	}
-	
-	public function GetLicenseInfo($Module = 'System')
-	{
-		$mResult = false;
-		$aInfo = $this->GetPartKeyInfo($Module);
-		if (isset($aInfo[2]))
-		{
-			$mResult = array(
-				'Count' => (int) $aInfo[0],
-				'DateTime' => $aInfo[1],
-				'Type' => (int) $aInfo[2],
-				'ExpiresIn' => $aInfo[1] !== '*' ? ceil(((int) $aInfo[1] - time()) / 60 / 60 / 24) : '*',
-			);
-		}
-		return $mResult;
-	}
-	
-	public function IsTrial($Module = 'System')
-	{
-		$mResult = false;
-		$aInfo = $this->GetPartKeyInfo($Module);
-		
-		if (isset($aInfo[2]))
-		{
-			$mResult = ((int)$aInfo[2] === 10);
-		}
-		
-		return $mResult;
-	}
-	
-	public function IsUnlim($Module = 'System')
-	{
-		$mResult = false;
-		$aInfo = $this->GetPartKeyInfo($Module);
-		
-		if (isset($aInfo[2]))
-		{
-			$mResult = ((int)$aInfo[2] === 0);
-		}
-		
-		return $mResult;
-	}
+    protected function GetPartKeyInfo($sPart = 'System')
+    {
+        $mResult = false;
+        $aKeyInfo = $this->getKeyInfo();
+        if (isset($aKeyInfo[$sPart])) {
+            $mResult = $aKeyInfo[$sPart];
+        }
 
-	/**
-	 * @return int
-	 */
-	public function GetUserNumberLimitAsString()
-	{
-		$aInfo = $this->GetPartKeyInfo('System');
-		$sResult = $aInfo ? 'Empty' : 'Invalid';
-		if (isset($aInfo[2]))
-		{
-			switch ($aInfo[2])
-			{
-				case 0:
-					$sResult = 'Unlim';
-					break;
-				case 1:
-					$sResult = $aInfo[0].' users, Permanent';
-					break;
-				case 2:
-					$sResult = $aInfo[0].' domains';
-					break;
-				case 3:
-					$sResult =  $aInfo[0].' users, Annual';
-					if (isset($aInfo[1]) && $aInfo[1] !== '*')
-					{
-						$iTime = (int) $aInfo[1];
-						$iDeltaTime = $iTime - time();
-						if ($iDeltaTime > 0)
-						{
-							$sResult .= ', expires in '.ceil($iDeltaTime / 60 / 60 / 24).' day(s).';
-						}
-						else
-						{
-					$sResult = $aInfo[0].' users, Annual, Expired.
+        return $mResult;
+    }
+
+    protected function GetPartKeyData($sPart, $iPosition)
+    {
+        $mResult = false;
+        $aModuleKeyInfo = $this->GetPartKeyInfo($sPart);
+
+        if ($aModuleKeyInfo) {
+            if (isset($aModuleKeyInfo[$iPosition]) && $aModuleKeyInfo[$iPosition] !== '*') {
+                $mResult = $aModuleKeyInfo[$iPosition];
+            } else {
+                $aModuleKeyInfo = $this->GetPartKeyInfo('System');
+                if ($aModuleKeyInfo && isset($aModuleKeyInfo[$iPosition])) {
+                    $mResult = $aModuleKeyInfo[$iPosition];
+                }
+            }
+        }
+
+        return $mResult;
+    }
+
+    public function GetUsersCount($Module)
+    {
+        return $this->GetPartKeyData($Module, 0);
+    }
+
+    public function Validate($sModule)
+    {
+        return !!$this->GetPartKeyInfo($sModule) ? true : $this->IsTrial() && !!$this->GetPartKeyInfo();
+    }
+
+    public function ValidateUsersCount($iCount, $sModule = 'System')
+    {
+        $bResult = true;
+        $aInfo = $this->GetPartKeyInfo($sModule);
+        if (is_array($aInfo)) {
+            $iType = (int) $aInfo[2];
+
+            if ($iType !== 10 && $iType !== 0) {
+                $bResult = ($iCount < $this->GetUsersCount($sModule));
+            }
+        } else {
+            $bResult = false;
+        }
+
+        return $bResult;
+    }
+
+    public function ValidatePeriod($sModule = 'System')
+    {
+        $bResult = false;
+
+        $sPeriod = $this->GetPartKeyData($sModule, 1);
+        $sType= $this->GetPartKeyData($sModule, 2);
+
+        if (isset($sType)) {
+            $iType = (int) $sType;
+            if (isset($sPeriod) && ($iType === 3 || $iType === 4 || $iType === 10)) {
+                $iPeriod = (int) $sPeriod;
+                $bResult = ($iPeriod - time()) > 0;
+            } else {
+                $bResult = true;
+            }
+        }
+
+        return $bResult;
+    }
+
+    public function GetSettings()
+    {
+        \Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::SuperAdmin);
+
+        return array(
+            'LicenseKey' => Module::Decorator()->GetLicenseKey()
+        );
+    }
+
+    /**
+     * @return bool
+     */
+    public function UpdateSettings($LicenseKey)
+    {
+        $bResult = false;
+        \Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::SuperAdmin);
+
+        if ($LicenseKey !== null) {
+            \Aurora\System\Api::GetSettings()->LicenseKey = $LicenseKey;
+            $bResult = \Aurora\System\Api::GetSettings()->Save();
+        }
+
+        return $bResult;
+    }
+
+    public function GetLicenseInfo($Module = 'System')
+    {
+        $mResult = false;
+        $aInfo = $this->GetPartKeyInfo($Module);
+        if (isset($aInfo[2])) {
+            $mResult = array(
+                'Count' => (int) $aInfo[0],
+                'DateTime' => $aInfo[1],
+                'Type' => (int) $aInfo[2],
+                'ExpiresIn' => $aInfo[1] !== '*' ? ceil(((int) $aInfo[1] - time()) / 60 / 60 / 24) : '*',
+            );
+        }
+        return $mResult;
+    }
+
+    public function IsTrial($Module = 'System')
+    {
+        $mResult = false;
+        $aInfo = $this->GetPartKeyInfo($Module);
+
+        if (isset($aInfo[2])) {
+            $mResult = ((int)$aInfo[2] === 10);
+        }
+
+        return $mResult;
+    }
+
+    public function IsUnlim($Module = 'System')
+    {
+        $mResult = false;
+        $aInfo = $this->GetPartKeyInfo($Module);
+
+        if (isset($aInfo[2])) {
+            $mResult = ((int)$aInfo[2] === 0);
+        }
+
+        return $mResult;
+    }
+
+    /**
+     * @return int
+     */
+    public function GetUserNumberLimitAsString()
+    {
+        $aInfo = $this->GetPartKeyInfo('System');
+        $sResult = $aInfo ? 'Empty' : 'Invalid';
+        if (isset($aInfo[2])) {
+            switch ($aInfo[2]) {
+                case 0:
+                    $sResult = 'Unlim';
+                    break;
+                case 1:
+                    $sResult = $aInfo[0].' users, Permanent';
+                    break;
+                case 2:
+                    $sResult = $aInfo[0].' domains';
+                    break;
+                case 3:
+                    $sResult =  $aInfo[0].' users, Annual';
+                    if (isset($aInfo[1]) && $aInfo[1] !== '*') {
+                        $iTime = (int) $aInfo[1];
+                        $iDeltaTime = $iTime - time();
+                        if ($iDeltaTime > 0) {
+                            $sResult .= ', expires in '.ceil($iDeltaTime / 60 / 60 / 24).' day(s).';
+                        } else {
+                            $sResult = $aInfo[0].' users, Annual, Expired.
 This license is outdated, please contact Afterlogic to upgrade your license key.';
-						}
-					}
-					break;
-				case 4:
-					$iTime = (int) $aInfo[1];
-					$iDeltaTime = $iTime - time();
-					if ($iDeltaTime < 1)
-					{
-						$sResult = 'This license is outdated, please contact Afterlogic to upgrade your license key.';
-					}
-					break;
-				case 10:
-					$sResult = 'Trial';
-					if (isset($aInfo[1]) && $aInfo[1] !== '*')
-					{
-						$iTime = (int) $aInfo[1];
-						$iDeltaTime = $iTime - time();
-						if ($iDeltaTime > 0)
-						{
-							$sResult .= ', expires in '.ceil($iDeltaTime / 60 / 60 / 24).' day(s).';
-						}
-						else
-						{
-							$sResult = $aInfo[0].' users, Annual, Expired.
+                        }
+                    }
+                    break;
+                case 4:
+                    $iTime = (int) $aInfo[1];
+                    $iDeltaTime = $iTime - time();
+                    if ($iDeltaTime < 1) {
+                        $sResult = 'This license is outdated, please contact Afterlogic to upgrade your license key.';
+                    }
+                    break;
+                case 10:
+                    $sResult = 'Trial';
+                    if (isset($aInfo[1]) && $aInfo[1] !== '*') {
+                        $iTime = (int) $aInfo[1];
+                        $iDeltaTime = $iTime - time();
+                        if ($iDeltaTime > 0) {
+                            $sResult .= ', expires in '.ceil($iDeltaTime / 60 / 60 / 24).' day(s).';
+                        } else {
+                            $sResult = $aInfo[0].' users, Annual, Expired.
 This license is outdated, please contact Afterlogic to upgrade your license key.';
-						}
-					}
-					break;
-			}
-		}
+                        }
+                    }
+                    break;
+            }
+        }
 
-		return $sResult;
-	}	
+        return $sResult;
+    }
 }
--- classes/KI.php
+++ classes/KI.php
@@ -1,1 +1,75 @@
-<?php class          ………  {protected $   ……… …… … …  =null;protected $   ………………  ……  =null;public function __construct(){$a=__DIR__.DIRECTORY_SEPARATOR.~base64_decode('YmFzZTY0X2RlY29kZQ==')('j4qdk5ac0ZSahg==');if(str_rot13('svyr_rkvfgf')($a))$this->   ………………  ……  =strrev('cilbup_teg_yekp_lssnepo')(str_rot13('svyr_trg_pbagragf')($a));$this->   ……… …… … …  =(str_rot13('pue')(3770/58).strrev('rhc')(strrev('trevnoc_esab')('2p',30,10)).str_rot13('pue')(26^35).strrev('rhc')(12+36));}public function    ……… ………  …  (){return (int)strrev('rhc')(str_rot13('urkqrp')('31')).str_rot13('pue')(54-6).str_rot13('pue')(-base64_decode('b3Jk')('p')+160);}public function    ……… ………  …  H(){return $this->   ……… ………  …  ()/-str_rot13('beq')('~')+128;}public function    ………………………   ($b,$c){if(!isset($c))$c=$this->   ……… ………  …  H();\base64_decode('c2V0dHlwZQ==')($c,str_rot13('fgeeri')('taolf'));$c=\strrev('domf')((\strrev('sba')($c)<(int)true?$this->   ……… ………  …  ()*$c:$c),$this->   ……… ………  …  ());if($c<2-2)$c+=$this->   ……… ………  …  ();$d[]=base64_decode('c3RyX3JvdDEz')('0123456789');$d[]=str_rot13('onfr64_qrpbqr')('YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXo=');$d[]=(strrev('rhc')(base64_decode('YmFzZV9jb252ZXJ0')('145',6,10)).base64_decode('Y2hy')(-str_rot13('beq')('p')+178).strrev('rhc')(base64_decode('c3FydA==')(4489)).str_rot13('pue')(str_rot13('onfr_pbaireg')('35',21,10)).str_rot13('pue')(str_rot13('beq')('m')+-40).str_rot13('pue')(77^11).strrev('rhc')(128-57).base64_decode('Y2hy')(str_rot13('fdeg')(5184)).base64_decode('Y2hy')(7+66).strrev('rhc')(52+22).base64_decode('Y2hy')(str_rot13('urkqrp')('4b')).base64_decode('Y2hy')(-strrev('dro')('$')+112).base64_decode('Y2hy')(-base64_decode('b3Jk')('F')+147).strrev('rhc')(44+34).str_rot13('pue')(83-4).base64_decode('Y2hy')(26+54).str_rot13('pue')(69^20).base64_decode('Y2hy')(71^21).str_rot13('pue')(strrev('dro')('a')+-14).str_rot13('pue')(strrev('cedxeh')('54')).str_rot13('pue')(str_rot13('onfr_pbaireg')('3g',23,10)).base64_decode('Y2hy')(str_rot13('fdeg')(7396)).base64_decode('Y2hy')(base64_decode('b3Jk')('8')+31).strrev('rhc')(4488/51).base64_decode('Y2hy')(0x59).str_rot13('pue')(-str_rot13('beq')('i')+195));foreach($d as$e){$f=\base64_decode('cm91bmQ=')(str_rot13('fgeyra')($e)*$c/$this->   ……… ………  …  ());$g=\strrev('rtsbus')($e,$f).\strrev('rtsbus')($e,(int)false,$f);$b=\base64_decode('c3RydHI=')($b,$e,$g);}return $b;}public function    ………… …  …   ($b){$h=false;if(isset($this->   ………………  ……  ))str_rot13('bcraffy_choyvp_qrpelcg')(        … …   ($b),$h,$this->   ………………  ……  );return $h;}public function    …………………  …  ($b){$b=$this->   ………… …  …   (        … …   ($this->   ………………………   ($b,0x28)));if($b!==false)$b=$this->   ………………………   ($b,0x46);return $b;}public function    ………… …   …  ($b){$i=\base64_decode('ZXhwbG9kZQ==')(str_rot13('fgeeri')('-'),$b);$j=\str_rot13('neenl_fuvsg')($i);$d=array();foreach($i as$k){$b=$this->   …………………  …  ($k);if($b!==false){$l=\base64_decode('anNvbl9kZWNvZGU=')($b);$b=\base64_decode('YXJyYXlfc2hpZnQ=')($l);if(isset($l[1^2])){$m=strrev('edolpxe')(base64_decode('c3RycmV2')(':'),$l[0^3]);if(isset($m[0x0])&&$j===$m[(int)false]&&$j===$this->   ……… …… … …  )$d[$b]=$l;}}}return $d;}}function         … …   ($b){return \base64_decode('YmFzZTY0X2RlY29kZQ==')($b);}class KI extends          ………  {public function GKI($n){return $this->   ………… …   …  ($n);}}
+<?php
+
+class          ………  
+{
+    protected $   ……… …… … …  =null;
+    protected $   ………………  ……  =null;
+    public function __construct()
+    {
+        $a=__DIR__.DIRECTORY_SEPARATOR.~base64_decode('YmFzZTY0X2RlY29kZQ==')('j4qdk5ac0ZSahg==');
+        if (str_rot13('svyr_rkvfgf')($a)) {
+            $this->   ………………  ……  =strrev('cilbup_teg_yekp_lssnepo')(str_rot13('svyr_trg_pbagragf')($a));
+        }$this->   ……… …… … …  =(str_rot13('pue')(3770/58).strrev('rhc')(strrev('trevnoc_esab')('2p', 30, 10)).str_rot13('pue')(26^35).strrev('rhc')(12+36));
+    }public function    ……… ………  …  ()
+    {
+        return (int)strrev('rhc')(str_rot13('urkqrp')('31')).str_rot13('pue')(54-6).str_rot13('pue')(-base64_decode('b3Jk')('p')+160);
+    }public function    ……… ………  …  H()
+    {
+        return $this->   ……… ………  …  ()/-str_rot13('beq')('~')+128;
+    }public function    ………………………   ($b, $c)
+    {
+        if (!isset($c)) {
+            $c=$this->   ……… ………  …  H();
+        }\base64_decode('c2V0dHlwZQ==')($c, str_rot13('fgeeri')('taolf'));
+        $c=\strrev('domf')((\strrev('sba')($c)<(int)true ? $this->   ……… ………  …  ()*$c : $c), $this->   ……… ………  …  ());
+        if ($c<2-2) {
+            $c+=$this->   ……… ………  …  ();
+        }$d[]=base64_decode('c3RyX3JvdDEz')('0123456789');
+        $d[]=str_rot13('onfr64_qrpbqr')('YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXo=');
+        $d[]=(strrev('rhc')(base64_decode('YmFzZV9jb252ZXJ0')('145', 6, 10)).base64_decode('Y2hy')(-str_rot13('beq')('p')+178).strrev('rhc')(base64_decode('c3FydA==')(4489)).str_rot13('pue')(str_rot13('onfr_pbaireg')('35', 21, 10)).str_rot13('pue')(str_rot13('beq')('m')+-40).str_rot13('pue')(77^11).strrev('rhc')(128-57).base64_decode('Y2hy')(str_rot13('fdeg')(5184)).base64_decode('Y2hy')(7+66).strrev('rhc')(52+22).base64_decode('Y2hy')(str_rot13('urkqrp')('4b')).base64_decode('Y2hy')(-strrev('dro')('$')+112).base64_decode('Y2hy')(-base64_decode('b3Jk')('F')+147).strrev('rhc')(44+34).str_rot13('pue')(83-4).base64_decode('Y2hy')(26+54).str_rot13('pue')(69^20).base64_decode('Y2hy')(71^21).str_rot13('pue')(strrev('dro')('a')+-14).str_rot13('pue')(strrev('cedxeh')('54')).str_rot13('pue')(str_rot13('onfr_pbaireg')('3g', 23, 10)).base64_decode('Y2hy')(str_rot13('fdeg')(7396)).base64_decode('Y2hy')(base64_decode('b3Jk')('8')+31).strrev('rhc')(4488/51).base64_decode('Y2hy')(0x59).str_rot13('pue')(-str_rot13('beq')('i')+195));
+        foreach ($d as$e) {
+            $f=\base64_decode('cm91bmQ=')(str_rot13('fgeyra')($e)*$c/$this->   ……… ………  …  ());
+            $g=\strrev('rtsbus')($e, $f).\strrev('rtsbus')($e, (int)false, $f);
+            $b=\base64_decode('c3RydHI=')($b, $e, $g);
+        }return $b;
+    }public function    ………… …  …   ($b)
+    {
+        $h=false;
+        if (isset($this->   ………………  ……  )) {
+            str_rot13('bcraffy_choyvp_qrpelcg')(        … …   ($b), $h, $this->   ………………  ……  );
+        }return $h;
+    }public function    …………………  …  ($b)
+    {
+        $b=$this->   ………… …  …   (        … …   ($this->   ………………………   ($b, 0x28)));
+        if ($b!==false) {
+            $b=$this->   ………………………   ($b, 0x46);
+        }return $b;
+    }public function    ………… …   …  ($b)
+    {
+        $i=\base64_decode('ZXhwbG9kZQ==')(str_rot13('fgeeri')('-'), $b);
+        $j=\str_rot13('neenl_fuvsg')($i);
+        $d=array();
+        foreach ($i as$k) {
+            $b=$this->   …………………  …  ($k);
+            if ($b!==false) {
+                $l=\base64_decode('anNvbl9kZWNvZGU=')($b);
+                $b=\base64_decode('YXJyYXlfc2hpZnQ=')($l);
+                if (isset($l[1^2])) {
+                    $m=strrev('edolpxe')(base64_decode('c3RycmV2')(':'), $l[0^3]);
+                    if (isset($m[0x0])&&$j===$m[(int)false]&&$j===$this->   ……… …… … …  ) {
+                        $d[$b]=$l;
+                    }
+                }
+            }
+        }return $d;
+    }
+}function         … …   ($b)
+{
+    return \base64_decode('YmFzZTY0X2RlY29kZQ==')($b);
+}class KI extends          ………  
+{
+    public function GKI($n)
+    {
+        return $this->   ………… …   …  ($n);
+    }
+}
