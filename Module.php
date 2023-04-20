8386e5fe00737764ca276ae8a2b163c31c0e428f
Vasil Sokolov <vasil@afterlogic.com>
2023-04-20 15:14:36 +0200

    Refactored getting module settings (part 3)

--- Module.php
+++ Module.php
@@ -31,7 +31,14 @@ class Module extends \Aurora\System\Module\AbstractModule
     }
 
     /**
-     *
+     * @return Module
+     */
+    public static function getInstance()
+    {
+        return parent::getInstance();
+    }
+
+    /**
      * @return Module
      */
     public static function Decorator()
@@ -40,7 +47,6 @@ class Module extends \Aurora\System\Module\AbstractModule
     }
 
     /**
-     *
      * @return Settings
      */
     public function getModuleSettings()
